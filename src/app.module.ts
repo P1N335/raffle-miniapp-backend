091923c3d3fddfea06159f2b5c8a455a30bc8d30
Nikitazaborrskikh <zaborskikh03@gmail.com>
2026-04-03 22:12:04 +0000

    а

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { resolve } from 'node:path';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { RaffleModule } from './raffle/raffle.module';
 import { AuthModule } from './auth/auth.module';
 
+const envFilePaths = [
+  resolve(process.cwd(), '.env'),
+  resolve(__dirname, '../.env'),
+  resolve(__dirname, '../../.env'),
+];
+
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: envFilePaths,
+    }),
     PrismaModule,
     UsersModule,
     RaffleModule,
